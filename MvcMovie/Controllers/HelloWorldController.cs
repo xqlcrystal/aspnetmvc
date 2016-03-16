c8af21c5ad5e71c30f6c0e4af9046ed7dfe664c5
xqlcrystal <xql252660@163.com>
2016-03-16 21:25:01 +0800

    aspnetmvc

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        public string Index()
+        {
+            return "This is my <b>Default</b> aciont";
+        }
+
+        public string Welcome(string name ,int ID = 3)
+        {
+            return HttpUtility.HtmlEncode("Hello "+name+" Numtimes is "+ID);
+        }
+    }
+}
